80e63a9a3636532ee5fbbb80bc1bf168183ea887
JoseACd744 <u20211d744@upc.edu.pe>
2024-04-13 08:53:19 -0500

    asasasa

---
+++ SuperHeroCompose/app/src/main/java/pe/edu/upc/superherocompose/model/remote/HeroService.kt
@@ -0,0 +1,16 @@
+package pe.edu.upc.superherocompose.model.remote
+
+import pe.edu.upc.superherocompose.model.data.HeroWrapper
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface HeroService {
+
+    @GET("{token}/search/{name}")
+    fun searchHero(
+        @Path("token") token: String = "10157703717092094",
+        @Path("name") name: String
+    ): Call<HeroWrapper>
+
+}
